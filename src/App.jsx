ac62923cc1ad6617f70901107e7b8bf4136d87a9
Faraji Ombonya <farajiombonya@gmail.com>
2024-08-26 22:57:25 +0300

    feat: add articles page

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import "./App.css";
 import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
 import ProjectsPage from "./pages/ProjectsPage";
+import ArticlesPage from "./pages/ArticlesPage";
 import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
 
 function App() {
-
   return (
     <>
       <HashRouter>
@@ -17,6 +17,7 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route index element={<HomePage />}></Route>
             <Route path="about" element={<AboutPage />}></Route>
+            <Route path="articles" element={<ArticlesPage />}></Route>
             <Route path="projects" element={<ProjectsPage />}></Route>
           </Route>
         </Routes>
--- src/pages/ArticlesPage.jsx
+++ src/pages/ArticlesPage.jsx
@@ -1,1 +1,94 @@
-export default function ArticlesPaage() {}
+export default function ArticlesPage() {
+  return (
+    <main class="flex-auto">
+      <div class="sm:px-8 mt-16 sm:mt-32">
+        <div class="mx-auto w-full max-w-7xl lg:px-8">
+          <div class="relative px-4 sm:px-8 lg:px-12">
+            <div class="mx-auto max-w-2xl lg:max-w-5xl">
+              <header class="max-w-2xl">
+                <h1 class="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl ">
+                  Writing on software engineering and the tech industry.
+                </h1>
+                <p class="mt-6 text-base text-zinc-600 ">
+                  My featured long-form thoughts on software engineering and
+                  related topics.
+                </p>
+              </header>
+              <div class="mt-16 sm:mt-20">
+                <div class="md:border-l md:border-zinc-100 md:pl-6 ">
+                  <div class="flex max-w-3xl flex-col space-y-16">
+                    <article class="md:grid md:grid-cols-4 md:items-baseline">
+                      <div class="md:col-span-3 group relative flex flex-col items-start">
+                        <h2 class="text-base font-semibold tracking-tight text-zinc-800 ">
+                          <div class="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl "></div>
+                          <a
+                            target="_blank"
+                            href="https://faraji-ombonya.github.io/blog/#/how-i-deployed-my-app-on-gh-pages"
+                          >
+                            <span class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
+                            <span class="relative z-10">
+                              How I Deployed my React Application with GitHub
+                              Pages
+                            </span>
+                          </a>
+                        </h2>
+                        <time
+                          class="md:hidden relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400  pl-3.5"
+                          datetime="2024-08-26"
+                        >
+                          <span
+                            class="absolute inset-y-0 left-0 flex items-center"
+                            aria-hidden="true"
+                          >
+                            <span class="h-4 w-0.5 rounded-full bg-zinc-200 "></span>
+                          </span>
+                          August 26, 2024
+                        </time>
+                        <p class="relative z-10 mt-2 text-sm text-zinc-600 ">
+                          After weeks of massive effort, I finally created my
+                          portfolio website and needed a place to deploy. The
+                          deployment options I had were Vercel, s3 site on AWS
+                          and github pages. I decided to go with github pages
+                          because it was free and also simple to deploy. GitHub
+                          Pages also had simple CI/CD already integrated. This
+                          meant that after a successful setup, all I needed to
+                          do was push to production and my application would be
+                          deployed.
+                        </p>
+                        <div
+                          aria-hidden="true"
+                          class="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500"
+                        >
+                          Read article
+                          <svg
+                            viewBox="0 0 16 16"
+                            fill="none"
+                            aria-hidden="true"
+                            class="ml-1 h-4 w-4 stroke-current"
+                          >
+                            <path
+                              d="M6.75 5.75 9.25 8l-2.5 2.25"
+                              stroke-width="1.5"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                            ></path>
+                          </svg>
+                        </div>
+                      </div>
+                      <time
+                        class="mt-1 hidden md:block relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 "
+                        datetime="2022-09-05"
+                      >
+                        August 26, 2024
+                      </time>
+                    </article>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
