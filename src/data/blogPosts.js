2ca54ea7ba6fb294d63c3424968c09ec0b5e32f3
Faraji Ombonya <farajiombonya@gmail.com>
2024-09-08 18:20:59 +0300

    feat: render articles dynamically

---
+++ src/data/blogPosts.js
@@ -0,0 +1,32 @@
+export const blogPosts = [
+  {
+    url: "https://faraji-ombonya.github.io/blog/#/how-to-train-a-word2vec-model",
+    title: "How to Train a Word2vec Model",
+    description:
+      "In this article, I'll show you how to train a Word2vec model using custom data. You can then use the trained model to perform tasks such as similarity search or powering a recommendation engine.",
+    date: {
+      display: "September 07, 2024",
+      datetime: "2024-09-07",
+    },
+  },
+  {
+    url: "https://faraji-ombonya.github.io/blog/#/how-i-built-a-library-search-engine-powered-by-vector-search",
+    title: "How I Built a Library Search Engine Powered by Vector Search",
+    description:
+      "Keyword search sometimes is not enough. Sometimes you want to search based on meaning. This is where vector search comes in. In this article, I want to take you through how I built a Library Search Engine that is powered by vector search.",
+    date: {
+      display: "September 2, 2024",
+      datetime: "2024-08-26",
+    },
+  },
+  {
+    url: "https://faraji-ombonya.github.io/blog/#/how-i-deployed-my-app-on-gh-pages",
+    title: "How I Deployed my React Application with GitHub Pages",
+    description:
+      "After weeks of massive effort, I finally created my portfolio website and needed a place to deploy. The deployment options I had were Vercel, s3 site on AWS and github pages. I decided to go with github pages because it was free and also simple to deploy. GitHub Pages also had simple CI/CD already integrated. This meant that after a successful setup, all I needed to do was push to production and my application would be deployed.",
+    date: {
+      display: "August 26, 2024",
+      datetime: "2024-08-26",
+    },
+  },
+];
--- src/pages/ArticlesPage.jsx
+++ src/pages/ArticlesPage.jsx
@@ -1,3 +1,5 @@
+import { blogPosts } from "../data/blogPosts";
+
 export default function ArticlesPage() {
   return (
     <main class="flex-auto">
@@ -17,132 +19,61 @@ export default function ArticlesPage() {
               <div class="mt-16 sm:mt-20">
                 <div class="md:border-l md:border-zinc-100 md:pl-6 ">
                   <div class="flex max-w-3xl flex-col space-y-16">
-                    <article class="md:grid md:grid-cols-4 md:items-baseline">
-                      <div class="md:col-span-3 group relative flex flex-col items-start">
-                        <h2 class="text-base font-semibold tracking-tight text-zinc-800 ">
-                          <div class="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl "></div>
-                          <a
-                            target="_blank"
-                            href="https://faraji-ombonya.github.io/blog/#/how-i-built-a-library-search-engine-powered-by-vector-search"
+                    {blogPosts?.map((article) => (
+                      <article class="md:grid md:grid-cols-4 md:items-baseline">
+                        <div class="md:col-span-3 group relative flex flex-col items-start">
+                          <h2 class="text-base font-semibold tracking-tight text-zinc-800 ">
+                            <div class="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl "></div>
+                            <a target="_blank" href={article?.url}>
+                              <span class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
+                              <span class="relative z-10">
+                                {article?.title}
+                              </span>
+                            </a>
+                          </h2>
+                          <time
+                            class="md:hidden relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400  pl-3.5"
+                            datetime={article?.date?.datetime}
                           >
-                            <span class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
-                            <span class="relative z-10">
-                              How I Built a Library Search Engine Powered by
-                              Vector Search
+                            <span
+                              class="absolute inset-y-0 left-0 flex items-center"
+                              aria-hidden="true"
+                            >
+                              <span class="h-4 w-0.5 rounded-full bg-zinc-200 "></span>
                             </span>
-                          </a>
-                        </h2>
-                        <time
-                          class="md:hidden relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400  pl-3.5"
-                          datetime="2024-08-26"
-                        >
-                          <span
-                            class="absolute inset-y-0 left-0 flex items-center"
+                            {article?.date?.display}
+                          </time>
+                          <p class="relative z-10 mt-2 text-sm text-zinc-600 ">
+                            {article?.description}
+                          </p>
+                          <div
                             aria-hidden="true"
+                            class="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500"
                           >
-                            <span class="h-4 w-0.5 rounded-full bg-zinc-200 "></span>
-                          </span>
-                          September 2, 2024
-                        </time>
-                        <p class="relative z-10 mt-2 text-sm text-zinc-600 ">
-                          Keyword search sometimes is not enough. Sometimes you
-                          want to search based on meaning. This is where vector
-                          search comes in. In this article, I want to take you
-                          through how I built a Library Search Engine that is
-                          powered by vector search.
-                        </p>
-                        <div
-                          aria-hidden="true"
-                          class="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500"
-                        >
-                          Read article
-                          <svg
-                            viewBox="0 0 16 16"
-                            fill="none"
-                            aria-hidden="true"
-                            class="ml-1 h-4 w-4 stroke-current"
-                          >
-                            <path
-                              d="M6.75 5.75 9.25 8l-2.5 2.25"
-                              stroke-width="1.5"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                            ></path>
-                          </svg>
+                            Read article
+                            <svg
+                              viewBox="0 0 16 16"
+                              fill="none"
+                              aria-hidden="true"
+                              class="ml-1 h-4 w-4 stroke-current"
+                            >
+                              <path
+                                d="M6.75 5.75 9.25 8l-2.5 2.25"
+                                stroke-width="1.5"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                              ></path>
+                            </svg>
+                          </div>
                         </div>
-                      </div>
-                      <time
-                        class="mt-1 hidden md:block relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 "
-                        datetime="2022-09-05"
-                      >
-                        September 2, 2024
-                      </time>
-                    </article>
-                    <article class="md:grid md:grid-cols-4 md:items-baseline">
-                      <div class="md:col-span-3 group relative flex flex-col items-start">
-                        <h2 class="text-base font-semibold tracking-tight text-zinc-800 ">
-                          <div class="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl "></div>
-                          <a
-                            target="_blank"
-                            href="https://faraji-ombonya.github.io/blog/#/how-i-deployed-my-app-on-gh-pages"
-                          >
-                            <span class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
-                            <span class="relative z-10">
-                              How I Deployed my React Application with GitHub
-                              Pages
-                            </span>
-                          </a>
-                        </h2>
                         <time
-                          class="md:hidden relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400  pl-3.5"
-                          datetime="2024-08-26"
+                          class="mt-1 hidden md:block relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 "
+                          datetime="2022-09-05"
                         >
-                          <span
-                            class="absolute inset-y-0 left-0 flex items-center"
-                            aria-hidden="true"
-                          >
-                            <span class="h-4 w-0.5 rounded-full bg-zinc-200 "></span>
-                          </span>
-                          August 26, 2024
+                          {article?.date?.display}
                         </time>
-                        <p class="relative z-10 mt-2 text-sm text-zinc-600 ">
-                          After weeks of massive effort, I finally created my
-                          portfolio website and needed a place to deploy. The
-                          deployment options I had were Vercel, s3 site on AWS
-                          and github pages. I decided to go with github pages
-                          because it was free and also simple to deploy. GitHub
-                          Pages also had simple CI/CD already integrated. This
-                          meant that after a successful setup, all I needed to
-                          do was push to production and my application would be
-                          deployed.
-                        </p>
-                        <div
-                          aria-hidden="true"
-                          class="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500"
-                        >
-                          Read article
-                          <svg
-                            viewBox="0 0 16 16"
-                            fill="none"
-                            aria-hidden="true"
-                            class="ml-1 h-4 w-4 stroke-current"
-                          >
-                            <path
-                              d="M6.75 5.75 9.25 8l-2.5 2.25"
-                              stroke-width="1.5"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                            ></path>
-                          </svg>
-                        </div>
-                      </div>
-                      <time
-                        class="mt-1 hidden md:block relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 "
-                        datetime="2022-09-05"
-                      >
-                        August 26, 2024
-                      </time>
-                    </article>
+                      </article>
+                    ))}
                   </div>
                 </div>
               </div>
